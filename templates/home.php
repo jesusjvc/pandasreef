e5b2aa5acaea39faaf01b23af58a44ef41a3066a
Maria del Mar Ramonell <marimar_r2003@hotmail.com>
2018-09-05 17:50:02 -0500

    actualizacion

--- templates/home.php
+++ templates/home.php
@@ -37,11 +37,98 @@
     </div>
 </section>
 
-<section>
+<section id="slider">
     <div class="slider">
-        <br>holahoa
+        <br>holahola
+         
     </div>
 </section>
+
+<section id="contacto" class="prueba">
+     <h2><span class="tipo">¡Contáctanos</span> <br><span class="interesa">hoy mismo!</span></h2>
+     <p class="contacto_letra">Coméntenos que desea para brindarle una mejor atención. </p>
+    <hr>
+    <div class="columna_contacto"> 
+       <div><br></div>
+        <div> 
+              <input type="checkbox" > Quiero un acuario a medida<br>
+              <input type="checkbox" > Quiero un acuario prefabricado<br>
+              <input type="text" placeholder="Tu nombre"><br>
+              <input type="email" placeholder="Tu correo"><br>
+              <textarea type="text" placeholder="Mensaje"></textarea><br>
+              <button>Ver catálogo</button>
+        
+        
+        </div>
+        <div>
+            <img src="<?php echo get_stylesheet_directory_uri(); ?>/img/correo.png" alt="Correo"> &nbsp;contacto@pandasreef.com<br>
+            
+            <img src="<?php echo get_stylesheet_directory_uri(); ?>/img/telefono.png" alt="Teléfono"> &nbsp;5522726203<br>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;5581455952<br>
+            <img src="<?php echo get_stylesheet_directory_uri(); ?>/img/tiempo.png" alt="Horario"> &nbsp;09:00 a.m. a 11:00 p.m.<br>
+            <img src="<?php echo get_stylesheet_directory_uri(); ?>/img/lugar.png" alt="Lugar"> &nbsp;Rocas 185, Jardines del pedregal, <br>AlvaroObregon, CDMX, <br>CP:01900<br>
+            <p><br>&nbsp;Síguenos en:&nbsp;&nbsp;<img src="<?php echo get_stylesheet_directory_uri(); ?>/img/redes.png" alt="Redes Sociales"> </p>
+        </div>
+        
+    
+    </div>
+</section>
+	<section id="amparo">
+    <div class="flex">
+        <div class="columna">
+            <img src="<?php echo get_stylesheet_directory_uri(); ?>/img/mcl/librosApilados.png" alt="Libros apilados">
+        </div>
+        <div class="columna">
+            <h2>¿Necesita un amparo?</h2>
+            <p>Todo acto de autoridad violatorio de derechos humanos debe ser restituido o debe la autoridad cumplir con lo que el derecho exija. 
+            </p>
+            <button>Contáctenos</button>
+        </div>
+    </div>
+</section>
+ <section id="mcl">
+    <div class="flex">
+        <div class="columna">
+            <img src="<?php echo get_stylesheet_directory_uri(); ?>/img/MCL.png" alt="MCL">
+            <h1>Magna Carta Lex</h1>
+            <p class="sup">Somos una firma de abogadas y abogados especializada en amparo, así como en derechos humanos y en temas constitucionales y referentes a las soberanías de los Estados, en México y/o en los diversos mecanismos internacionales, como por ejemplo la Corte Internacional de Justicia, la Comisión Interamericana de Derechos Humanos o la Corte Interamericana de Derechos Humanos, que trata a sus clientas y clientes de manera personalizada, en un entorno de familiaridad.</p>
+            <p class="inf">Nuestros productos y servicios jurídicos pasan por un riguroso proceso de perfeccionamiento que tiene como destino la excelencia.</p>
+        </div>
+        <div class="columna">
+            <img src="<?php echo get_stylesheet_directory_uri(); ?>/img/mcl/rey.png" alt="Rey bicolor ajedrez">
+        </div>
+    </div>
+</section>
+ <section id="pro">
+    <h2>
+        <i>Pro bono</i>
+        y equidad de género
+    </h2>
+    <p class="sup">Creemos en la constante necesidad de impulsar el estado de las cosas hacia adelante, y en la equidad de género, que, en lo que nos compete, desemboca en la autonomía de las personas respecto a la plena defensa de sus derechos y en el trato equitativo a quienes forman parte de nuestra firma.</p>
+    <p class="inf">/</p>
+    <img src="<?php echo get_stylesheet_directory_uri(); ?>/img/mcl/equidadGenero.png" alt="balanza con hombre y mujer frente a la ciudad">
+</section>
+ <section id="blog">
+    <h2>Blog</h2>
+    <div class="flex">
+        <div class="columna"><img src="<?php echo get_stylesheet_directory_uri(); ?>/img/mcl/law.png" alt="libro de leyes y martillo de juez"></div>
+        <div class="columna"><img src="<?php echo get_stylesheet_directory_uri(); ?>/img/mcl/librosFeosApilados.png" alt="libros apilados"></div>
+        <div class="columna"><img src="<?php echo get_stylesheet_directory_uri(); ?>/img/mcl/martilloColor.png" alt="martillo de juez"></div>
+    </div>
+</section>
+
+ <section id="suscribir">
+    <div class="flex">
+        <div class="columna"></div>
+        <div class="columna">
+            <h2>Suscríbase a nuestro blog</h2>
+            <input type="text" placeholder="Introduzca su correo electrónico">
+            <button>Suscribirme</button>
+        </div>
+    </div>
+</section>
+ <?php 
+    get_footer();
+?>
 
 <?php 
     get_footer();
