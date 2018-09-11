a5c42287ba8738ea4177dced1af936ca8a011623
Maria del Mar Ramonell <marimar_r2003@hotmail.com>
2018-09-11 12:49:11 -0500

    quienes_somos

--- templates/quienes.php
+++ templates/quienes.php
@@ -2,51 +2,73 @@
     /* Template Name: Quienes*/ 
     get_header();
 ?>
+
+    <section id="quienes_somos" class="template-section">
+
+        <div class="contenedor">
+            <h2 class="tipo"><i>¿Quiénes somos?</i></h2>
+            <div class="grid">
+                <div class="col">
+                    <img src="<?php echo get_stylesheet_directory_uri(); ?>/img/acerca.png" alt="Acerca">
+                </div>
+                <div class="col">
+                    <h2><i>Panda’s Reef</i></h2>
+                    <p class="azul">
+                        Somos una empresa con 15 años de experiencia en el diseño y construcción de acuarios marinos arquitectónicos y prefabricados. 
+                        
+                       
+                    </p>
+                    <p class="azul">El aprecio por la vida marina es nuestra pasión por eso nos empeñamos en crear acuarios con la mejor calidad y tecnología para satisfacer a nuestros clientes.   </p>
+                    
+                </div>
+            </div>
+        </div>
+
+    </section>
+
+    <section id="mision" class="template-section">
+        <div class="grid">
+            <div class="col">
+                <h2>Misión</h2>
+                <p class="txt-azul">
+                    Crear acuarios marinos domésticos e industriales con sistemas funcionales y tecnologías avanzadas que representen una solución completa, sencilla y real a nuestros clientes.
+                </p>
+            </div>
+            <div class="col fondo"></div>
+        </div>
+    </section>
+        <section id="vision" class="template-section">
+        <div class="grid">
+            <div class="col fondo"></div>
+            <div class="col">
+                <h2>Visión</h2>
+                <p class="txt-azul">
+                    Ser la empresa de construcción y diseño de acuarios marinos domésticos e industriales número uno en México y Latinoamérica, entregando la mejor calidad y diseño en todos nuestros trabajos.
+                </p>
+            </div>
+           
+        </div>
+    </section>
+
+
+
+
+
+
+
 <section>
-        
-         <h2 class="quienes_somos">¿Quienes somos?</h2>
-    <div class="tipo_acuario">
-       <div> <br></div>
-        <div class="columna columna1">
-            <img src="<?php echo get_stylesheet_directory_uri(); ?>/img/acerca.png" alt="Diseño Medida">
-        </div>
-        <div class="columna columna2">
-            
-            <p class="diseño2">Somos una empresa con 15 años de experiencia en el diseño y construcción de acuarios marinos arquitectónicos y prefabricados. </p>
- 
-<p class="diseño2">El aprecio por la vida marina es nuestra pasión por eso nos empeñamos en crear acuarios con la mejor calidad y tecnología para satisfacer a nuestros clientes.   </p>
-    
-            
-            
-        </div>
-        <div> <br></div>
+    <div id="valores" class="template-section">
+         <h2 class="valores_titulo"><i>Valores</i></h2>
+         <div class="grid">
+         <div  class="col2"><img src="<?php echo get_stylesheet_directory_uri(); ?>/img/seriedad.png" alt="seriedad">
+         
+         <p>SERIEDAD</p>
+         </div>
+          <div  class="col2"><img src="<?php echo get_stylesheet_directory_uri(); ?>/img/compromiso.png" alt="compromiso">
+           <p>COMPROMISO</p></div>
+         <div  class="col2"><img src="<?php echo get_stylesheet_directory_uri(); ?>/img/honestidad.png" alt="honestidad">
+          <p>HONESTIDAD</p></div>
     </div>
-</section>
-<section class="quienes_somos">
-  <div class="acerca_sec">
-   <div >
-    <h2 class="mision">Misión</h2>
-    <p class="acuario_acerca">Crear acuarios marinos domésticos e industriales con sistemas funcionales y tecnologías avanzadas que representen una solución completa, sencilla y real a nuestros clientes.
-</p></div>
-   <div>
-       <img src="<?php echo get_stylesheet_directory_uri(); ?>/img/Hotel-Lobby_Aquarium.png" alt="acerca_medida">
-   </div>
-   </div>
-      <div class="acerca_sec2">
-      <div>
-       <img src="<?php echo get_stylesheet_directory_uri(); ?>/img/pecera.png" alt="pecera">
-   </div>
-   <div>
-    <h2 class="vision">Visión</h2>
-    <p class="acuario_acerca2">Diseñamos y construimos acuarios marinos arquitectónicos.  Ideales para decorar espacios de lujo en empresas (Hoteles, restaurantes, clubs, oficinas, consultorios, entre otros) y hogares. 
-</p></div>
-   </div>
-</section>
-<section>
-    <div class="valores">
-         <div ><img src="<?php echo get_stylesheet_directory_uri(); ?>/img/seriedad.png" alt="seriedad"></div>
-          <div ><img src="<?php echo get_stylesheet_directory_uri(); ?>/img/compromiso.png" alt="compromiso"></div>
-         <div ><img src="<?php echo get_stylesheet_directory_uri(); ?>/img/honestidad.png" alt="honestidad"></div>
     </div>
 </section>
 
