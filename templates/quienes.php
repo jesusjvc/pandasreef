ff048b0de12883c647643dab7c6c6fc15b222a20
Maria del Mar Ramonell <marimar_r2003@hotmail.com>
2018-09-10 19:15:05 -0500

    actualizacion

---
+++ templates/quienes.php
@@ -0,0 +1,55 @@
+<?php
+    /* Template Name: Quienes*/ 
+    get_header();
+?>
+<section>
+        
+         <h2 class="quienes_somos">¿Quienes somos?</h2>
+    <div class="tipo_acuario">
+       <div> <br></div>
+        <div class="columna columna1">
+            <img src="<?php echo get_stylesheet_directory_uri(); ?>/img/acerca.png" alt="Diseño Medida">
+        </div>
+        <div class="columna columna2">
+            
+            <p class="diseño2">Somos una empresa con 15 años de experiencia en el diseño y construcción de acuarios marinos arquitectónicos y prefabricados. </p>
+ 
+<p class="diseño2">El aprecio por la vida marina es nuestra pasión por eso nos empeñamos en crear acuarios con la mejor calidad y tecnología para satisfacer a nuestros clientes.   </p>
+    
+            
+            
+        </div>
+        <div> <br></div>
+    </div>
+</section>
+<section class="quienes_somos">
+  <div class="acerca_sec">
+   <div >
+    <h2 class="mision">Misión</h2>
+    <p class="acuario_acerca">Crear acuarios marinos domésticos e industriales con sistemas funcionales y tecnologías avanzadas que representen una solución completa, sencilla y real a nuestros clientes.
+</p></div>
+   <div>
+       <img src="<?php echo get_stylesheet_directory_uri(); ?>/img/Hotel-Lobby_Aquarium.png" alt="acerca_medida">
+   </div>
+   </div>
+      <div class="acerca_sec2">
+      <div>
+       <img src="<?php echo get_stylesheet_directory_uri(); ?>/img/pecera.png" alt="pecera">
+   </div>
+   <div>
+    <h2 class="vision">Visión</h2>
+    <p class="acuario_acerca2">Diseñamos y construimos acuarios marinos arquitectónicos.  Ideales para decorar espacios de lujo en empresas (Hoteles, restaurantes, clubs, oficinas, consultorios, entre otros) y hogares. 
+</p></div>
+   </div>
+</section>
+<section>
+    <div class="valores">
+         <div ><img src="<?php echo get_stylesheet_directory_uri(); ?>/img/seriedad.png" alt="seriedad"></div>
+          <div ><img src="<?php echo get_stylesheet_directory_uri(); ?>/img/compromiso.png" alt="compromiso"></div>
+         <div ><img src="<?php echo get_stylesheet_directory_uri(); ?>/img/honestidad.png" alt="honestidad"></div>
+    </div>
+</section>
+
+<?php 
+    get_footer();
+?>
